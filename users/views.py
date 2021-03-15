0695312f484a26a67f32feb8f763caa664486642
x038xx77 <x038xx77@gmail.com>
2021-03-15 21:01:08 +0300

    correct mistake reviews two part

--- users/views.py
+++ users/views.py
@@ -1,9 +1,15 @@
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from .forms import CreationForm
+from django.contrib.auth import login
 
 
 class SignUp(CreateView):
     form_class = CreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+    success_url = reverse_lazy("index")
+    template_name = "signup.html"
+
+    def form_valid(self, form):
+        valid = super().form_valid(form)
+        login(self.request, self.object)
+        return valid
